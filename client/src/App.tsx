f84fe6a0c95740c896e602a534a5b63fdae49ecf
Jennifer H Thompson <writejthompson@gmail.com>
2025-02-06 19:15:25 -0600

    new base files from scratch

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './src/pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<div>Login Page (Coming Soon)</div>} />
+        <Route path="/dashboard" element={<div>Dashboard (Coming Soon)</div>} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
